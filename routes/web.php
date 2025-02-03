ae58bb34f7814f6fa62533d17b5dd2c5dcd5d69e
MatteoPC <mnmartinez@fiuna.edu.py>
2025-02-03 15:26:54 -0300

    modified:   bootstrap/app.php 	modified:   routes/web.php Incio 2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/redirect', function (Request $request) {
+//     $request->session()->put('state', $state = Str::random(40));
+
+//     $query = http_build_query([
+//         'client_id' => '3',
+//         'redirect_uri' => 'http://localhost/auth/callback',
+//         'response_type' => 'code',
+//         'scope' => '',
+//         'state' => $state,
+//         // 'prompt' => '', // "none", "consent", or "login"
+//     ]);
+
+//     return redirect('http://localhost:8000/oauth/authorize?'.$query);
+// });
